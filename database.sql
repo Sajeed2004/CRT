0cc5cd6faaaa8b831c5a814adc972beae20ba42f
Venuthurla Mohammad Sajeed <venuthurlasajeed@gmail.com>
2025-08-16 09:25:49 +0530

    Delete database.sql

--- database.sql
+++
@@ -1,8 +0,0 @@
-create database expenses_tracker;
-use expenses_tracker;
-create table expenses(
-	serial_num int primary key,
-    category varchar(500),
-    item varchar(500),
-    cost varchar(100)
-    );
